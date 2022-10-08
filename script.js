f2862d4cd79fb6055c39344d3bddc3cbb01b3662
Wissam Mansour <wissman77@gmail.com>
2022-10-08 03:46:45 +0300

    Add playRound method to determine a winner, and test it.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const rock = 'Rock';
-const paper = 'Paper';
-const scissors = 'Scissors';
+const rock = 'rock';
+const paper = 'paper';
+const scissors = 'scissors';
 
 function getComputerChoice() {
   // create array with all game values and return a random value
@@ -9,5 +9,27 @@ function getComputerChoice() {
   return gameValues[randomIndex];
 }
 
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  
+  switch(true) {
+    case playerSelection === computerSelection:
+      return 'It\'s Tie!';
+    case playerSelection === rock && computerSelection === paper:
+      return 'You Lose! Paper beats Rock';
+    case playerSelection === rock && computerSelection === scissors:
+      return 'You Win! Rock beats Scissors';
+    case playerSelection === paper && computerSelection === rock:
+      return 'You Win! Paper beats Rock';
+    case playerSelection === paper && computerSelection === scissors:
+      return 'You Lose! Scissors beats Paper';
+    case playerSelection === scissors && computerSelection === rock:
+      return 'You Lose! Rock beats Scissors';
+    case playerSelection === scissors && computerSelection === paper:
+      return 'You Win! Scissors beats Paper';
+  }
+}
 
-console.log(getComputerChoice());
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
